1836f5f7afcf7a8acfbfd03ce615a13e47b13a36
HasiruAmarasooriya <89748072+HasiruAmarasooriya@users.noreply.github.com>
2023-01-25 18:54:08 +0530

    Add files via upload
    add all

---
+++ test 2/test2/test2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text== "Fahrenheit to Celcius")
+            {
+                double f = double.Parse(textBox1.Text);
+                double c;
+
+                c = (f - 32) * 5 / 9;
+
+                textBox2.Text = c.ToString();
+
+            }
+            else if (comboBox1.Text == "Celcius to Fahrenheit") 
+            {
+                double c = double.Parse(textBox1.Text);
+                double f;
+
+               f = ( c * 9 / 5) + 32;
+
+                textBox2.Text = f.ToString();
+            }
+            else 
+            {
+                textBox2.Text = "Please Select one from the above list";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                double f = double.Parse(textBox3.Text);
+                double c;
+
+                c = (f - 32) * 5 / 9;
+
+                textBox4.Text = c.ToString();
+            }
+            else if (radioButton2.Checked == true) 
+            {
+                double c = double.Parse(textBox3.Text);
+                double f;
+
+                f = (c * 9 / 5) + 32;
+
+                textBox4.Text = f.ToString();
+            }
+            else
+            {
+                textBox4.Text = "Please Select one from the above list";
+            }
+        }
+    }
+}
